5c319eaa442236df53f65761f616203f0c9751b3
Water Pug <david.lupien@polymtl.ca>
2020-04-28 14:32:34 -0400

    Update BIOS and Ram with faster read/writes

--- src/Architecture/Ram.cpp
+++ src/Architecture/Ram.cpp
@@ -14,18 +14,12 @@ namespace ePugStation
 
 	uint16_t Ram::load16(uint32_t offset) const
 	{
-		uint32_t b0 = static_cast<uint32_t>(m_data[offset]);
-		uint32_t b1 = static_cast<uint32_t>(m_data[offset + 1]);
-		return	b0 | (b1 << 8);
+		return static_cast<uint16_t>(((uint16_t*)(m_data.data()))[offset >> 1]);
 	}
 
 	uint32_t Ram::load32(uint32_t offset) const
 	{
-		auto b0 = static_cast<uint32_t>(m_data[offset]);
-		auto b1 = static_cast<uint32_t>(m_data[offset + 1]);
-		auto b2 = static_cast<uint32_t>(m_data[offset + 2]);
-		auto b3 = static_cast<uint32_t>(m_data[offset + 3]);
-		return	b0 | (b1 << 8) | (b2 << 16) | (b3 << 24);
+		return static_cast<uint32_t>(((uint32_t*)(m_data.data()))[offset >> 2]);
 	}
 
 	void Ram::store8(uint32_t offset, uint8_t value)
@@ -35,15 +29,11 @@ namespace ePugStation
 
 	void Ram::store16(uint32_t offset, uint16_t value)
 	{
-		m_data[offset] = static_cast<uint8_t>(value);
-		m_data[offset + 1] = static_cast<uint8_t>(value >> 8);
+		((uint16_t*)(m_data.data()))[offset >> 1] = value;
 	}
 
 	void Ram::store32(uint32_t offset, uint32_t value)
 	{
-		m_data[offset] = static_cast<uint8_t>(value);
-		m_data[offset + 1] = static_cast<uint8_t>(value >> 8);
-		m_data[offset + 2] = static_cast<uint8_t>(value >> 16);
-		m_data[offset + 3] = static_cast<uint8_t>(value >> 24);
+		((uint32_t*)(m_data.data()))[offset >> 2] = value;
 	}
 }
